007126567bea3d328644b4bea233c9cbf53950b1
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2025-08-20 08:14:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="imagescraper-mavi",
-    version="0.1.0",
+    version="0.1.1",
     author="MaviMods",
     author_email="your-email@example.com",
     description="A simple Python package to scrape images from Google, Bing, and Yahoo",
